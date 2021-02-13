fdb1f2e8ddfc2bbd9dd8d13ff141ae34b174c682
AylenMoreno <aylu.more1503@gmail.com>
2021-02-13 20:22:45 -0300

    Fundamentos de Router terminados

--- router-udemy-1/src/views/Articulo.vue
+++ router-udemy-1/src/views/Articulo.vue
@@ -3,6 +3,8 @@
      <Titulo texto="Ruta con parametros" />
      <!-- Mostramos el parametro que recibe por url -->
      <h2>Parámetro: {{ $route.params.id }}</h2> 
+     <h3>{{ articulo.title }}</h3>
+     <p>{{articulo.body}}</p>
   </div>
 </template>
 
@@ -12,6 +14,25 @@ import Titulo from '../components/Titulo'
 export default {
    components: {
       Titulo
+   },
+   data(){
+      return{
+         articulo: {}
+      }
+   },
+   methods: {
+      async consumirArticulo(){
+         try {
+            const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${this.$route.params.id}`);
+            const objeto = await data.json();
+            this.articulo = objeto;
+         } catch (error) {
+            console.log(error);
+         }
+      }
+   },
+   created(){
+      this.consumirArticulo();
    }
 }
 </script>
